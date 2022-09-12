cdfe4d83cab4543e1d93897e2470a78e81dbd055
luisjaas <luisjaas98@gmail.com>
2022-09-12 05:31:05 +0000

    proyecto domain name genetaro

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
   console.log("Hello Rigo from the console!");
 };
+
+let pronun = ["the", "our", "me", "is", "her"];
+let adj = ["great", "big", "best", "cool", "funy"];
+let noun = ["jogger", "racoon", "car", "dog", "show"];
+let domain = [".com", ".net", ".ve", ".es", ".org"];
+
+for (let i = 0; i < pronun.length; i++) {
+  console.log(pronun[i]);
+}
+console.log(domain.length);
+
+function getDomain() {
+  for (let i = 0; i < pronun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < domain.length; d++) {
+          let x = Math.floor(Math.random() * 15);
+          console.log(pronun[i] + adj[j] + noun[k] + domain[x]);
+        }
+      }
+    }
+  }
+}
